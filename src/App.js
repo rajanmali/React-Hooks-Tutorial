e898cd4382562460dd90ee71efd669dfd5ca3cb7
Rajan Mali <mysteriouskiller50@gmail.com>
2020-11-17 17:52:09 +0545

    changes to footer styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import "./App.css";
-// import ClassCounter from "./components/useState/ClassCounter";
 import UseStateArray from "./components/useState/UseStateArray";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounter /> */}
       <UseStateArray />
-      <footer style={{ position: "fixed", bottom: "0" }}>
-        <p>
-          <a
-            href="https://github.com/rajanmali"
-            target="_blank"
-            rel="noreferrer"
-            className="App-link"
-          >
-            @notrajanmali
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,44 @@
+const Footer = () => {
+  return (
+    <footer className="App-Footer">
+      <ul>
+        <li style={{ color: "white" }}>Find me at:</li>
+        &nbsp;
+        <li>
+          <a
+            href="https://github.com/rajanmali"
+            target="_blank"
+            rel="noreferrer"
+            className="App-link"
+          >
+            Github
+          </a>
+          &nbsp;{"."}&nbsp;
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/notrajanmali"
+            target="_blank"
+            rel="noreferrer"
+            className="App-link"
+          >
+            Twitter
+          </a>
+          &nbsp;{"."}&nbsp;
+        </li>
+        <li>
+          <a
+            href="https://instagram.com/notrajanmali"
+            target="_blank"
+            rel="noreferrer"
+            className="App-link"
+          >
+            Instagram
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
